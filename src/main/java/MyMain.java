0a1d8b3abf683b002b1bc37e910e01c11eb363cf
ethanarnold <emailtoethan@gmail.com>
2021-09-08 22:17:07 -0700

    made a string checker that checks if a string contains the, is a palindrome, and tells you how many a's b's and c's it contains

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,26 +4,43 @@ public class MyMain {
     
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        int abc = 0;
+        for(int i = 0; i<str.length(); i++) {
+            if(str.charAt(i)=='a') abc++;
+            if(str.charAt(i)=='b') abc++;
+            if(str.charAt(i)=='c') abc++;
+        }
+        return abc;
     }
 
-    // Checks to see whether or not str contains 'The' or 'the'
+    // Checks to see whether str contains 'The' or 'the'
     public static boolean containsThe(String str) {
-        // REPLACE WITH YOUR CODE
-        return false;
+        return(str.contains("The") || str.contains("the"));
     }
 
     // Checks whether str is a palindrome or not
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
-        return false;
+        String reverse = "";
+        for(int i = str.length()-1; i>=0; i--) {
+            reverse += str.charAt(i);
+        }
+        return (str.equalsIgnoreCase(reverse));
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("please input a string bro");
+        String str = scan.nextLine();
+        System.out.println("it has " + countABC(str) + " a's, b's, and c's");
+        if(containsThe(str)) {
+            System.out.println("it contains the or The.");
+        }
+        else System.out.println("it does not contain the or The.");
 
-        // YOUR CODE HERE
+        if(isPalindrome(str)) {
+            System.out.println("it is a palindrome.");
+        }
+        else System.out.println("it is not a palindrome");
     }
 }
